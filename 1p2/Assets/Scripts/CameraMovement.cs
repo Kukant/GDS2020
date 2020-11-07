53fea67320ecc3df310bfa17aacce5f3f1dab198
Kuky <Kukant96@gmail.com>
2020-11-07 01:58:56 +0100

    Initial scene, camera movement...

---
+++ 1p2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    
+    private Transform playerTransform;        //Public variable to store a reference to the player game object
+    private float offset = 3;
+    private float maxXoff = 3;
+    private Camera thisCamera;
+
+    // Use this for initialization
+    void Start () {
+        thisCamera = GetComponent<Camera>();
+        // do this in Run method that will be called on clicking play
+        playerTransform = GameObject.Find("Packet").transform;
+    }
+
+    // LateUpdate is called after Update each frame
+    void FixedUpdate () {
+        var playerPos = playerTransform.position;
+        var idealPosition = new Vector3(playerPos.x + offset, playerPos.y, transform.position.z);
+        var yDistance = idealPosition.y - transform.position.y;
+        
+        transform.position = new Vector3(idealPosition.x, transform.position.y + 0.05f * yDistance, idealPosition.z);
+    }
+}
