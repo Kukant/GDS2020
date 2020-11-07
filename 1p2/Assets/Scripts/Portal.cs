0295b86a8f731474cee1cf2cb466bf93ef94545a
Marketa Sauerova <market.sauer@gmail.com>
2020-11-07 17:47:19 +0100

    Portal without particles kundy, still good

---
+++ 1p2/Assets/Scripts/Portal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Transform PortalOut;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.name == "Packet")
+        {
+            collision.gameObject.transform.position = PortalOut.position + new Vector3(2, 0, 0);
+        }
+    }
+}
