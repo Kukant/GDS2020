6dbcbaea613bba1b504fdaeedc60c3a34b5e5402
Kuky <Kukant96@gmail.com>
2020-11-07 11:04:30 +0100

    asdad

--- 1p2/Assets/Scripts/PacketMovement.cs
+++ 1p2/Assets/Scripts/PacketMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class packet : MonoBehaviour
+public class PacketMovement : MonoBehaviour
 {
     public float DefaultSpeed = 2.5f;
 
