a716e1914f57fa50b6d7a5df124fbee42b41bddb
Fezzzi <filip.horky.cz@gmail.com>
2020-11-07 18:30:00 +0100

    Create UI

--- 1p2/Assets/Scripts/GameController.cs
+++ 1p2/Assets/Scripts/GameController.cs
@@ -1,8 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
+    public bool DEBUG = false;
+
+    public GameObject Menu;
 
     public List<GameObject> levels;
 
@@ -14,7 +17,12 @@ public class GameController : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Save(10, 1);
+        if (!DEBUG) {
+            MenuStart();
+        } else {
+            Menu.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -22,4 +30,26 @@ public class GameController : MonoBehaviour {
     {
         
     }
+    
+    private void MenuStart() {
+        Menu.SetActive(true);
+    }
+    
+    public void Save(int score, int level) {
+        GameSaving data;
+        if (File.Exists(Application.persistentDataPath + "/saves.json"))
+            data = JsonUtility.FromJson<GameSaving>(File.ReadAllText(Application.persistentDataPath + "/saves.json"));
+        else {
+            data = new GameSaving();
+            data.scores = new []{ 0, 0, 0, 0 };
+        }
+        data.scores[level - 1] = score;
+        string jsonData = JsonUtility.ToJson (data, true);
+        File.WriteAllText (Application.persistentDataPath + "/saves.json", jsonData);
+        gameObject.GetComponentInChildren<MenuScript>().ReloadScores();
+    }
+}
+
+public class GameSaving {
+    public int[] scores;
 }
