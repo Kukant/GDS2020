60ad8a29a1fef26096c16702d5f9f14a955fa58e
Marketa Sauerova <market.sauer@gmail.com>
2020-11-07 13:59:42 +0100

    Displayed text in level prefab

---
+++ 1p2/Assets/Scripts/LevelText.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelText : MonoBehaviour
+{
+    public string DisplayedText = string.Empty;
+    private Text levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText = gameObject.GetComponent<Text>();
+        levelText.text = DisplayedText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeDisplayedText(string newText)
+    {
+        levelText.text = newText;
+    }
+}
