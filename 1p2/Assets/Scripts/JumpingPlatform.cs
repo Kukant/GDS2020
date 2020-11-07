461b49ff120c4b2d1e4c7a8f6f51d6c705e12e3a
Kuky <Kukant96@gmail.com>
2020-11-07 17:20:20 +0100

    Jumping platform

---
+++ 1p2/Assets/Scripts/JumpingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpingPlatform : MonoBehaviour {
+    private float force = 1000f;
+
+    private void OnTriggerStay2D(Collider2D other) {
+        var rb = other.GetComponent<Rigidbody2D>();
+        if (rb) {
+            rb.AddForce(Vector2.up * force);
+        }
+
+    }
+}
